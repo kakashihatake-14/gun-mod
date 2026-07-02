9839fedbeaf9b83920294ea1ae885f4107ef78f4
Hatake_gamin14 <julian.baronnn@gmail.com>
2026-07-02 23:12:35 +0200

    added a test item called pink_ganet

--- src/main/java/net/julian/gunmod/item/ModItems.java
+++ src/main/java/net/julian/gunmod/item/ModItems.java
@@ -6,10 +6,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item PINK_GARNET=registerItem("pink_garnet",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(GunMod.MOD_ID,"pink_garnet")))));
 
 
 private static Item registerItem(String name, Item item) {
